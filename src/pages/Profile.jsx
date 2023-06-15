170c138fd10c14747fce28997ba5725137540e33
Abhishek <67749693+abhishekY495@users.noreply.github.com>
2023-06-15 22:42:44 +0530

    Created Profile.jsx

---
+++ src/pages/Profile.jsx
@@ -0,0 +1,49 @@
+import React, { useContext } from "react";
+import { AuthContext } from "../contexts/AuthContext";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-hot-toast";
+
+export default function Profile() {
+  const { logoutUser } = useContext(AuthContext);
+  const currentUser = JSON.parse(localStorage.getItem("currentUser"));
+  const navigate = useNavigate();
+
+  const logoutBtnHandler = () => {
+    logoutUser();
+    navigate("/");
+    toast.success("Logged Out");
+  };
+
+  const { fullName, userName, email, followers, following } = currentUser;
+
+  return (
+    <div className="w-[320px] m-auto bg-slate-200 p-4">
+      <p>
+        <span className="font-medium">Name:</span>
+        {fullName}
+      </p>
+      <p>
+        <span className="font-medium">Username:</span>
+        {userName}
+      </p>
+      <p>
+        <span className="font-medium">Email:</span>
+        {email}
+      </p>
+      <div className="flex gap-4">
+        <p>
+          <b>{followers.length}</b>Followers
+        </p>
+        <p>
+          <b>{following.length}</b>Following
+        </p>
+      </div>
+      <button
+        className="bg-red-400 p-1 px-5 rounded my-2"
+        onClick={logoutBtnHandler}
+      >
+        Logout
+      </button>
+    </div>
+  );
+}
