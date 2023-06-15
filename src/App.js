841b4a05683b19f3e7b5b350f10f1f2394c2a63d
Abhishek <67749693+abhishekY495@users.noreply.github.com>
2023-06-15 22:40:34 +0530

    Created Sidebar components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
+import { Route, Routes } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+
+import Authentication from "./components/Authentication";
+import Profile from "./pages/Profile";
+import HomePage from "./pages/HomePage";
+import ProtectedRoute from "./components/ProtectedRoute";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div>
+      <Toaster position="top-center" />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <HomePage />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/signup" element={<Authentication signup />} />
+        <Route path="/login" element={<Authentication login />} />
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
     </div>
   );
 }
