88b3cbd43995fc2c7cabdac8e4ddce0e49cea670
Abhishek <67749693+abhishekY495@users.noreply.github.com>
2023-06-15 22:40:33 +0530

    Created Filters.jsx

---
+++ src/components/Filters.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Filters() {
+  return (
+    <div className="flex justify-center">
+      <button className="border-2 w-full">Latest</button>
+      <button className="border-2 w-full">Trending</button>
+    </div>
+  );
+}
