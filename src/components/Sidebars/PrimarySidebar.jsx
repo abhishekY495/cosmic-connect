4ee49c6b384552cfadd8ef75216c50f849192a80
Abhishek <67749693+abhishekY495@users.noreply.github.com>
2023-06-16 16:12:18 +0530

    Refractor

--- src/components/Sidebars/PrimarySidebar.jsx
+++ src/components/Sidebars/PrimarySidebar.jsx
@@ -3,14 +3,16 @@ import { Link } from "react-router-dom";
 
 export default function PrimarySidebar() {
   return (
-    <div className="w-[100px]">
-      <Link className="flex gap-1 fixed">
+    <div className="w-[220px] border-[1px] border-zinc-300 sticky top-0 h-screen flex flex-col">
+      <Link className="flex gap-1">
         <p>Cosmic</p>
         <img
           src="https://img.icons8.com/?size=24&id=GiHNxwe2ZmsA&format=svg"
           alt="cosmic connect"
         />
       </Link>
+      <Link to="/explore"># Explore</Link>
+      <Link to="/profile">Profile</Link>
     </div>
   );
 }
--- src/components/Sidebars/SecondarySidebar.jsx
+++ src/components/Sidebars/SecondarySidebar.jsx
@@ -2,8 +2,9 @@ import React from "react";
 
 export default function SecondarySidebar() {
   return (
-    <div className="w-[100px]">
-      <ul className="fixed">
+    <div className="w-[220px] border-[1px] border-zinc-300 sticky top-0 h-screen">
+      <input type="text" placeholder="Search Users..." className="border-[1px]"/>
+      <ul className="">
         <li>neiltyson</li>
         <li>abhishekY495</li>
         <li>3tweetSauce</li>
