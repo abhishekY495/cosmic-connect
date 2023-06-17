fdab8ef86b953cf679f6a41f785584fec2fbdc9e
Abhishek <67749693+abhishekY495@users.noreply.github.com>
2023-06-17 12:29:12 +0530

    Refactor: suggested users

--- src/components/Sidebars/SecondarySidebar.jsx
+++ src/components/Sidebars/SecondarySidebar.jsx
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useContext } from "react";
+
+import { AuthContext } from "../../contexts/AuthContext";
+import { UsersDataContext } from "../../contexts/UsersDataContext";
 
 export default function SecondarySidebar() {
+  const { currentUser } = useContext(AuthContext);
+  const { usersData } = useContext(UsersDataContext);
+
+  const followingUsers = currentUser.following;
+
+  const suggestedUsers = usersData.filter((user) => {
+    return (
+      !followingUsers.includes(user.userName) &&
+      !user.userName.includes(currentUser.userName)
+    );
+  });
+
   return (
     <div className="w-[220px] border-[1px] border-zinc-300 sticky top-0 h-screen">
-      <input type="text" placeholder="Search Users..." className="border-[1px]"/>
+      <input
+        type="text"
+        placeholder="Search Users..."
+        className="border-[1px]"
+      />
       <ul className="">
-        <li>neiltyson</li>
-        <li>abhishekY495</li>
-        <li>3tweetSauce</li>
-        <li>musicalscience</li>
+        {suggestedUsers.map((user) => {
+          return <li key={user.userName}>{user.userName}</li>;
+        })}
       </ul>
     </div>
   );
