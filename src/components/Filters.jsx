829c1c6f8d1f583980ddb28e739ef5b346406258
Abhishek <67749693+abhishekY495@users.noreply.github.com>
2023-06-17 12:29:12 +0530

    Feat: filter posts

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { useContext } from "react";
+
+import { PostsDataContext } from "../contexts/PostsDataContext";
 
 export default function Filters() {
+  const { state, dispatch } = useContext(PostsDataContext);
+  const { filterByCreatedAt, filterByTrending } = state;
+
+  const latestBtnHandler = () => {
+    dispatch({ type: "FILTER_BY_CREATED_AT" });
+  };
+  const trendingBtnHandler = () => {
+    dispatch({ type: "FILTER_BY_TRENDING" });
+  };
+
   return (
-    <div className="flex justify-center">
-      <button className="border-2 w-full">Latest</button>
-      <button className="border-2 w-full">Trending</button>
+    <div className="flex sticky top-6">
+      <button
+        onClick={latestBtnHandler}
+        className={`w-full ${
+          filterByCreatedAt
+            ? "bg-zinc-500/50 backdrop-blur-xl"
+            : "bg-zinc-300/50 backdrop-blur-xl"
+        }`}
+      >
+        Latest
+      </button>
+      <button
+        onClick={trendingBtnHandler}
+        className={`w-full ${
+          filterByTrending
+            ? "bg-zinc-500/50 backdrop-blur-xl"
+            : "bg-zinc-300/50 backdrop-blur-xl"
+        }`}
+      >
+        Trending
+      </button>
     </div>
   );
 }
