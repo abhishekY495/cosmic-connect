fa800cab75462c8e3f333a0db46914360fa1d3c2
Abhishek <67749693+abhishekY495@users.noreply.github.com>
2023-06-17 12:30:47 +0530

    Created NewPost.jsx

---
+++ src/components/NewPost.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function NewPost() {
+  return (
+    <input className="border-[1px] sticky top-0" type="text" placeholder="New Post" />
+  );
+}
--- src/components/Searchbar.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Searchbar() {
-  return <input className="py-2 border-[1px]" type="text" placeholder="Search Users" />;
-}
