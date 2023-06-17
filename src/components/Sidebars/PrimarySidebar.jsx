580d9406d56da512d266355a5c77318b176082cf
Abhishek <67749693+abhishekY495@users.noreply.github.com>
2023-06-17 12:29:12 +0530

    Refactor

--- src/components/Sidebars/PrimarySidebar.jsx
+++ src/components/Sidebars/PrimarySidebar.jsx
@@ -3,16 +3,18 @@ import { Link } from "react-router-dom";
 
 export default function PrimarySidebar() {
   return (
-    <div className="w-[220px] border-[1px] border-zinc-300 sticky top-0 h-screen flex flex-col">
-      <Link className="flex gap-1">
-        <p>Cosmic</p>
-        <img
-          src="https://img.icons8.com/?size=24&id=GiHNxwe2ZmsA&format=svg"
-          alt="cosmic connect"
-        />
+    <aside className="w-[220px] border-[1px] border-zinc-300 sticky top-0 h-screen flex flex-col gap-4 text-end">
+      <Link to="/home">
+        <div className="flex gap-1 justify-end">
+          <p>Cosmic</p>
+          <img
+            src="https://img.icons8.com/?size=24&id=GiHNxwe2ZmsA&format=svg"
+            alt="cosmic connect"
+          />
+        </div>
       </Link>
       <Link to="/explore"># Explore</Link>
       <Link to="/profile">Profile</Link>
-    </div>
+    </aside>
   );
 }
