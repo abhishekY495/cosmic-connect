93e335b061099ec38cc873a196c53b1fac6075aa
Abhishek <67749693+abhishekY495@users.noreply.github.com>
2023-06-18 23:29:34 +0530

    Refactor: checking username via useParams

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,19 @@
 import React, { useContext } from "react";
-import { AuthContext } from "../contexts/AuthContext";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { toast } from "react-hot-toast";
 
+import { AuthContext } from "../contexts/AuthContext";
+import PrimarySidebar from "../components/Sidebars/PrimarySidebar";
+import SecondarySidebar from "../components/Sidebars/SecondarySidebar";
+import { UsersDataContext } from "../contexts/UsersDataContext";
+
 export default function Profile() {
-  const { currentUser, logoutUser } = useContext(AuthContext);
+  const { logoutUser } = useContext(AuthContext);
+  const { usersData } = useContext(UsersDataContext);
+  const { username } = useParams();
   const navigate = useNavigate();
+
+  const user = usersData.find((user) => user.userName === username);
 
   const logoutBtnHandler = () => {
     logoutUser();
@@ -13,36 +21,44 @@ export default function Profile() {
     toast.success("Logged Out");
   };
 
-  const { fullName, userName, email, followers, following } = currentUser;
-
   return (
-    <div className="w-[320px] m-auto bg-slate-200 p-4">
-      <p>
-        <span className="font-medium">Name:</span>
-        {fullName}
-      </p>
-      <p>
-        <span className="font-medium">Username:</span>
-        {userName}
-      </p>
-      <p>
-        <span className="font-medium">Email:</span>
-        {email}
-      </p>
-      <div className="flex gap-4">
-        <p>
-          <b>{followers.length}</b>Followers
-        </p>
-        <p>
-          <b>{following.length}</b>Following
-        </p>
+    <div className="flex justify-center gap-10">
+      <PrimarySidebar />
+      <div className="w-[500px] bg-slate-200">
+        {!user ? (
+          <p className="font-medium text-center">Use profile not found.</p>
+        ) : (
+          <>
+            <p>
+              <span className="font-medium">Name:</span>
+              {user.fullName}
+            </p>
+            <p>
+              <span className="font-medium">Username:</span>
+              {user.userName}
+            </p>
+            <p>
+              <span className="font-medium">Email:</span>
+              {user.email}
+            </p>
+            <div className="flex gap-4">
+              <p>
+                <b>{user.followers.length}</b>Followers
+              </p>
+              <p>
+                <b>{user.following.length}</b>Following
+              </p>
+            </div>
+            <button
+              className="bg-red-400 p-1 px-5 rounded my-2"
+              onClick={logoutBtnHandler}
+            >
+              Logout
+            </button>
+          </>
+        )}
       </div>
-      <button
-        className="bg-red-400 p-1 px-5 rounded my-2"
-        onClick={logoutBtnHandler}
-      >
-        Logout
-      </button>
+      <SecondarySidebar />
     </div>
   );
 }
