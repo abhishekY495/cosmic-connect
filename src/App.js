776d91da11def48fb803b7cc4985860c769ad374
Abhishek <67749693+abhishekY495@users.noreply.github.com>
2023-06-20 15:50:39 +0530

    Fix: Added useEffect

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 
-import { AuthContext } from "./contexts/AuthContext";
 import Authentication from "./components/Authentication";
 import ProtectedRoute from "./components/ProtectedRoute";
 import HomePage from "./pages/HomePage";
 import ExplorePage from "./pages/ExplorePage";
 import Profile from "./pages/Profile";
 import SinglePost from "./components/SinglePost";
+import { AuthContext } from "./contexts/AuthContext";
+import { PostsDataContext } from "./contexts/PostsDataContext";
 
 function App() {
   const { currentUser } = useContext(AuthContext);
+  const { dispatch } = useContext(PostsDataContext);
+  const API_URL = "https://cosmic-connect-api.abhisheky495.repl.co/postsdata";
+
+  const getPosts = async () => {
+    dispatch({ type: "LOADING" });
+    try {
+      const response = await fetch(API_URL);
+      const data = await response.json();
+      dispatch({ type: "GET_POSTS_DATA", payload: data });
+      dispatch({ type: "FILTER_BY_CREATED_AT" });
+    } catch (error) {
+      dispatch({ type: "GET_POSTS_DATA_ERROR", payload: error });
+    }
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
   return (
     <div>
       <Toaster position="top-center" />
