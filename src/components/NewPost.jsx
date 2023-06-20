a7a9892d8450bce489f3e94a519953b25c4db897
Abhishek <67749693+abhishekY495@users.noreply.github.com>
2023-06-20 15:52:48 +0530

    Refactor: prop

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
-export default function NewPost() {
+export default function NewPost({ usersPost }) {
   return (
-    <input className="border-[1px] sticky top-0" type="text" placeholder="New Post" />
+    <input
+      className={`border-[1px] sticky ${usersPost ? "top-[104px]" : "top-0"}`}
+      type="text"
+      placeholder="New Post"
+    />
   );
 }
