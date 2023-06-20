cce2565847e4423fcc64aade78dca6df74a10ebe
Abhishek <67749693+abhishekY495@users.noreply.github.com>
2023-06-20 15:50:38 +0530

    Refactor: removed useEffect

--- src/pages/ExplorePage.jsx
+++ src/pages/ExplorePage.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext } from "react";
 
 import { PostsDataContext } from "../contexts/PostsDataContext";
 import PrimarySidebar from "../components/Sidebars/PrimarySidebar";
@@ -6,27 +6,9 @@ import SecondarySidebar from "../components/Sidebars/SecondarySidebar";
 import PostsListing from "../components/PostsListing";
 
 export default function ExplorePage() {
-  const API_URL = "https://cosmic-connect-api.abhisheky495.repl.co/postsdata";
   const {
     state: { postsData, postsLoading, postsDataError },
-    dispatch,
   } = useContext(PostsDataContext);
-
-  const getPosts = async () => {
-    dispatch({ type: "LOADING" });
-    try {
-      const response = await fetch(API_URL);
-      const data = await response.json();
-      dispatch({ type: "GET_POSTS_DATA", payload: data });
-      dispatch({ type: "FILTER_BY_CREATED_AT" });
-    } catch (error) {
-      dispatch({ type: "GET_POSTS_DATA_ERROR", payload: error });
-    }
-  };
-
-  useEffect(() => {
-    getPosts();
-  }, []);
 
   return (
     <div className="flex justify-center gap-10">
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 
 import SecondarySidebar from "../components/Sidebars/SecondarySidebar";
 import PrimarySidebar from "../components/Sidebars/PrimarySidebar";
@@ -7,10 +7,8 @@ import { PostsDataContext } from "../contexts/PostsDataContext";
 import { AuthContext } from "../contexts/AuthContext";
 
 export default function HomePage() {
-  const API_URL = "https://cosmic-connect-api.abhisheky495.repl.co/postsdata";
   const {
     state: { postsData, postsLoading, postsDataError },
-    dispatch,
   } = useContext(PostsDataContext);
   const { currentUser } = useContext(AuthContext);
   const currentUserFollowing = currentUser.following;
@@ -18,22 +16,6 @@ export default function HomePage() {
   const followingPosts = postsData.filter((post) => {
     return currentUserFollowing.includes(post.userName);
   });
-
-  const getPosts = async () => {
-    dispatch({ type: "LOADING" });
-    try {
-      const response = await fetch(API_URL);
-      const data = await response.json();
-      dispatch({ type: "GET_POSTS_DATA", payload: data });
-      dispatch({ type: "FILTER_BY_CREATED_AT" });
-    } catch (error) {
-      dispatch({ type: "GET_POSTS_DATA_ERROR", payload: error });
-    }
-  };
-
-  useEffect(() => {
-    getPosts();
-  }, []);
 
   return (
     <div className="flex justify-center gap-10">
