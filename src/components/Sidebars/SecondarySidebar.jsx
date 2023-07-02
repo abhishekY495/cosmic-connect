72393630ead5a14f0fb992bb3bf4ddf13bd29fc0
Abhishek <67749693+abhishekY495@users.noreply.github.com>
2023-07-02 13:28:17 +0530

    Feat: Search users

--- src/components/Sidebars/SecondarySidebar.jsx
+++ src/components/Sidebars/SecondarySidebar.jsx
@@ -1,46 +1,94 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
+import { Link } from "react-router-dom";
 
 import { AuthContext } from "../../contexts/AuthContext";
 import { UsersDataContext } from "../../contexts/UsersDataContext";
-import { Link } from "react-router-dom";
+import followIcon from "../../assets/profile/followIcon.svg";
+import verifiedIcon from "../../assets/profile/verified.svg";
 
 export default function SecondarySidebar() {
   const { currentUser } = useContext(AuthContext);
   const {
     state: { usersData },
+    dispatch,
   } = useContext(UsersDataContext);
+  const [searchTerm, setSearchTerm] = useState("");
 
-  const userProfile = usersData?.find(
+  const currentUserProfile = usersData?.find(
     (user) => user.userName === currentUser.userName
   );
-
-  const followingUsers = userProfile?.following;
+  const currentUsersFollowing = currentUserProfile?.following;
 
   const suggestedUsers = usersData?.filter((user) => {
     return (
-      !followingUsers
+      !currentUsersFollowing
         .map((followingUser) => followingUser.userName)
         .includes(user.userName) &&
       !user.userName.includes(currentUser.userName)
     );
   });
 
+  const searchedUsers = usersData.filter((user) => {
+    return (
+      user.userName.toLowerCase().includes(searchTerm) ||
+      user.fullName.toLowerCase().includes(searchTerm)
+    );
+  });
+
+  const usersList = searchTerm.length === 0 ? suggestedUsers : searchedUsers;
+
+  if (searchTerm.length === 0) {
+    usersList.sort((a, b) => b.followers.length - a.followers.length);
+  }
+
+  const followUser = (username) => {
+    dispatch({ type: "FOLLOW", payload: { username, currentUser } });
+  };
+
   return (
-    <div className="w-[220px] border-[1px] border-zinc-300 sticky top-0 h-screen">
+    <div className="w-[300px] border-l-[1px] sticky top-0 h-screen pl-3">
       <input
         type="text"
+        onChange={(e) => setSearchTerm(e.target.value)}
+        className="border-[1px] w-full rounded-md pl-1"
         placeholder="Search Users..."
-        className="border-[1px]"
       />
-      <ul className="">
-        {suggestedUsers.map((user) => {
-          return (
-            <Link key={user.userName} to={`/${user.userName}`}>
-              <li key={user.userName}>{user.userName}</li>
-            </Link>
-          );
+      <div>
+        {usersList.map((user, index) => {
+          if (index < 5) {
+            return (
+              <div
+                key={user.userName}
+                className="flex justify-between items-center"
+              >
+                <Link
+                  to={`/${user.userName}`}
+                  className="flex items-center gap-2"
+                >
+                  <img src={user.avatar} className="w-9" alt="" />
+                  <div>
+                    <div className="flex gap-1">
+                      <p className="w-max font-medium">{user.fullName}</p>
+                      {user.verified && (
+                        <img src={verifiedIcon} className="w-4" alt="" />
+                      )}
+                    </div>
+                    <p className="font-light text-sm text-gray-500">
+                      @{user.userName}
+                    </p>
+                  </div>
+                </Link>
+                <img
+                  src={followIcon}
+                  onClick={() => followUser(user.userName)}
+                  className="w-7 p-1 bg-zinc-600 rounded-full hover:cursor-pointer"
+                  alt=""
+                />
+              </div>
+            );
+          }
         })}
-      </ul>
+      </div>
     </div>
   );
 }
