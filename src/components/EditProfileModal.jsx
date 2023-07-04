bec016e48623675ce17c9c564d2ebcaa9bf976ad
Abhishek <67749693+abhishekY495@users.noreply.github.com>
2023-07-04 15:31:32 +0530

    Created EditProfileModal

---
+++ src/components/EditProfileModal.jsx
@@ -0,0 +1,123 @@
+import React, { useContext, useState } from "react";
+
+import { UsersDataContext } from "../contexts/UsersDataContext";
+import { AuthContext } from "../contexts/AuthContext";
+
+export default function EditProfileModal({ open, onClose, userProfile }) {
+  const { currentUser } = useContext(AuthContext);
+  const { dispatch } = useContext(UsersDataContext);
+  const [fullName, setFullName] = useState(userProfile && userProfile.fullName);
+  const [avatar, setAvatar] = useState(userProfile && userProfile.avatar);
+  const [bio, setBio] = useState(userProfile && userProfile.bio);
+  const [website, setWebsite] = useState(userProfile && userProfile.website);
+  const allowedFileExt = ["png", "jpeg", "jpg"];
+
+  const closeModal = () => {
+    onClose();
+    setAvatar(userProfile.avatar);
+    setFullName(userProfile.fullName);
+    setBio(userProfile.bio);
+    setWebsite(userProfile.website);
+  };
+
+  const handleMedia = (e) => {
+    const file = e.target.files[0];
+    let isAllowed = false;
+    allowedFileExt.forEach((ext) => {
+      if (file.type.includes(ext)) {
+        isAllowed = true;
+        setAvatar(URL.createObjectURL(file));
+      }
+    });
+    if (!isAllowed) {
+      alert("File not allowed");
+    }
+  };
+
+  const updateUserProfile = () => {
+    const updatedUserProfile = {
+      ...currentUser,
+      fullName,
+      bio,
+      avatar,
+      website,
+    };
+    dispatch({
+      type: "UPDATE_USER_PROFILE",
+      payload: { updatedUserProfile, currentUser },
+    });
+    closeModal();
+  };
+
+  if (!open) {
+    return null;
+  } else {
+    return (
+      <div
+        id="overlay-profile"
+        className="fixed top-0 right-0 bg-zinc-500/50 w-screen h-screen z-[2]"
+        onClick={closeModal}
+      >
+        <div
+          id="edit-profile-modal"
+          className="bg-white flex flex-col gap-3 w-[350px] m-auto p-5 mt-8"
+          onClick={(e) => e.stopPropagation()}
+        >
+          <img
+            src={avatar}
+            className="w-[50%] h-[160px] object-cover m-auto rounded-full"
+            alt="user profile"
+          />
+          <label
+            htmlFor="update-avatar"
+            className="relative hover:cursor-pointer"
+          >
+            <span className="absolute bg-yellow-400 rounded-full p-1 pt-0 bottom-3 right-20">
+              📷
+            </span>
+            <input
+              id="update-avatar"
+              type="file"
+              onChange={handleMedia}
+              className="hidden"
+              accept="image/png, image/jpeg, image/jpg"
+            />
+          </label>
+          <label>
+            Full Name
+            <input
+              className="border w-full pl-1"
+              type="text"
+              onChange={(e) => setFullName(e.target.value)}
+              defaultValue={fullName}
+            />
+          </label>
+          <label>
+            Bio
+            <textarea
+              className="border w-full pl-1"
+              cols="30"
+              onChange={(e) => setBio(e.target.value)}
+              defaultValue={bio}
+            ></textarea>
+          </label>
+          <label>
+            Website
+            <input
+              className="border w-full pl-1"
+              type="text"
+              onChange={(e) => setWebsite(e.target.value)}
+              defaultValue={website}
+            />
+          </label>
+          <button
+            className="bg-zinc-500 p-1 text-white rounded"
+            onClick={updateUserProfile}
+          >
+            Update Profile
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
