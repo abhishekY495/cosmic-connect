72e59b8a2b5d5d8e1bf8e1872c9cbaad145863d9
Abhishek <67749693+abhishekY495@users.noreply.github.com>
2023-07-05 21:14:33 +0530

    Refactor

--- src/components/PostFormModal.jsx
+++ src/components/PostFormModal.jsx
@@ -5,22 +5,28 @@ import { PostsDataContext } from "../contexts/PostsDataContext";
 import { AuthContext } from "../contexts/AuthContext";
 import crossIcon from "../assets/posts/cross-icon.svg";
 
-export default function PostFormModal({ open, onClose }) {
+export default function PostFormModal({ open, onClose, postToEdit }) {
   const { dispatch } = useContext(PostsDataContext);
   const {
     currentUser: { userName, fullName, avatar, verified },
   } = useContext(AuthContext);
-  const [postContent, setPostContent] = useState("");
-  const [postMedia, setPostMedia] = useState("");
-  const allowedFileExt = ["png", "gif", "jpeg", "jpg", "mp4", "mkv", "mpv"];
+  const [postContent, setPostContent] = useState(
+    postToEdit ? postToEdit?.content : ""
+  );
+  const [postMedia, setPostMedia] = useState(
+    postToEdit ? postToEdit?.media : ""
+  );
+  const [selectedFile, setSelectedFile] = useState(null);
+  const allowedFileExt = ["png", "gif", "jpeg", "jpg", "mp4"];
 
   const handleMedia = (e) => {
     const file = e.target.files[0];
+    setSelectedFile(file);
     let isAllowed = false;
     allowedFileExt.forEach((ext) => {
       if (file.type.includes(ext)) {
         isAllowed = true;
-        setPostMedia(file);
+        setPostMedia(URL.createObjectURL(file));
       }
     });
     if (!isAllowed) {
@@ -30,54 +36,40 @@ export default function PostFormModal({ open, onClose }) {
   };
 
   const closeModal = () => {
-    setPostMedia("");
-    setPostContent("");
+    !postToEdit && setPostMedia("") && setPostMedia("");
     onClose();
   };
 
   const uploadPost = () => {
-    const media = () => {
-      if (postMedia) {
-        const fileExt = postMedia.type;
-        if (fileExt.includes("mp4")) {
-          return {
-            image: "",
-            video: URL.createObjectURL(postMedia),
-          };
-        } else if (
-          fileExt.includes("png") ||
-          fileExt.includes("gif") ||
-          fileExt.includes("jpg") ||
-          fileExt.includes("jpeg")
-        ) {
-          return {
-            image: URL.createObjectURL(postMedia),
-            video: "",
-          };
-        }
-      } else {
-        return {
-          image: "",
-          video: "",
-        };
-      }
-    };
-    const newPost = {
-      id: uuidv4(),
-      userName,
-      fullName,
-      avatar,
-      verified,
-      content: postContent,
-      media: media(),
-      createdAt: new Date(),
-      updatedAt: "",
-      likedBy: [],
-      bookmarkedBy: [],
-      comments: [],
-    };
-    dispatch({ type: "CREATE_POST", payload: newPost });
-    closeModal();
+    if (postToEdit) {
+      const updatedPost = {
+        ...postToEdit,
+        content: postContent,
+        media: postMedia,
+        isVideo: selectedFile && selectedFile.type.includes("mp4") ? true : false,
+        edited: true,
+      };
+      dispatch({ type: "UPDATE_POST", payload: { postToEdit, updatedPost } });
+      closeModal();
+    } else {
+      const newPost = {
+        id: uuidv4(),
+        userName,
+        fullName,
+        avatar,
+        verified,
+        content: postContent,
+        media: postMedia,
+        isVideo: selectedFile.type.includes("mp4") ? true : false,
+        createdAt: new Date(),
+        updatedAt: "",
+        likedBy: [],
+        bookmarkedBy: [],
+        comments: [],
+      };
+      dispatch({ type: "CREATE_POST", payload: newPost });
+      closeModal();
+    }
   };
 
   if (!open) return null;
@@ -93,7 +85,7 @@ export default function PostFormModal({ open, onClose }) {
         onClick={(e) => e.stopPropagation()}
       >
         <div className="flex justify-between">
-          <p>New Post</p>
+          <p>{postToEdit ? "Edit Post" : "New Post"}</p>
           <img
             src={crossIcon}
             onClick={closeModal}
@@ -103,7 +95,8 @@ export default function PostFormModal({ open, onClose }) {
         </div>
         <textarea
           cols="50"
-          rows="3"
+          rows="5"
+          defaultValue={postContent}
           onChange={(e) => setPostContent(e.target.value)}
         ></textarea>
         <div className="flex justify-center">
@@ -115,16 +108,17 @@ export default function PostFormModal({ open, onClose }) {
                 className="absolute right-1 w-6 bg-slate-100/50 backdrop:blur-lg p-1 mt-1 rounded-full hover:cursor-pointer"
                 alt="delete"
               />
-              {postMedia.type.includes("mp4") ? (
+              {selectedFile?.type?.includes("mp4") || postToEdit?.isVideo ? (
                 <iframe
-                  src={URL.createObjectURL(postMedia)}
+                  src={postMedia}
                   className="w-full h-52"
                   title="user upload"
+                  allowFullScreen
                 ></iframe>
               ) : (
                 <img
-                  className="w-full h-52 object-cover"
-                  src={URL.createObjectURL(postMedia)}
+                  className="w-full h-52 object-contain bg-zinc-950"
+                  src={postMedia}
                   alt="user upload"
                 />
               )}
@@ -137,12 +131,12 @@ export default function PostFormModal({ open, onClose }) {
               className="hidden"
               type="file"
               id="file"
-              accept="image/png, image/gif, image/jpeg, image/jpg, video/mp4, video/mkv, video/mpv"
+              accept="image/png, image/gif, image/jpeg, image/jpg, video/mp4"
               onChange={handleMedia}
             />
             🖼️
           </label>
-          <button onClick={uploadPost}>Post</button>
+          <button onClick={uploadPost}>{postToEdit ? "Update" : "Post"}</button>
         </div>
       </div>
     </div>
