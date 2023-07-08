46d03aed333996672cec98b24e966b7f7e5f1af2
Abhishek <67749693+abhishekY495@users.noreply.github.com>
2023-07-08 20:29:19 +0530

    Refactor

--- src/components/Sidebars/SecondarySidebar.jsx
+++ src/components/Sidebars/SecondarySidebar.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 
 import { AuthContext } from "../../contexts/AuthContext";
@@ -13,34 +13,22 @@ export default function SecondarySidebar() {
     state: { usersData, usersDataLoading },
     dispatch,
   } = useContext(UsersDataContext);
-  const [searchTerm, setSearchTerm] = useState("");
 
   const currentUserProfile = usersData?.find(
     (user) => user.userName === currentUser.userName
   );
   const currentUsersFollowing = currentUserProfile?.following;
 
-  const suggestedUsers = usersData?.filter((user) => {
-    return (
-      !currentUsersFollowing
-        ?.map((followingUser) => followingUser.userName)
-        ?.includes(user.userName) &&
-      !user.userName.includes(currentUser.userName)
-    );
-  });
-
-  const searchedUsers = usersData?.filter((user) => {
-    return (
-      user.userName.toLowerCase().includes(searchTerm) ||
-      user.fullName.toLowerCase().includes(searchTerm)
-    );
-  });
-
-  const usersList = searchTerm.length === 0 ? suggestedUsers : searchedUsers;
-
-  if (searchTerm.length === 0) {
-    usersList.sort((a, b) => b.followers.length - a.followers.length);
-  }
+  const suggestedUsers = usersData
+    ?.filter((user) => {
+      return (
+        !currentUsersFollowing
+          ?.map((followingUser) => followingUser.userName)
+          ?.includes(user.userName) &&
+        !user.userName.includes(currentUser.userName)
+      );
+    })
+    .sort((a, b) => b.followers.length - a.followers.length);
 
   const followUser = (username) => {
     dispatch({ type: "FOLLOW", payload: { username, currentUser } });
@@ -48,13 +36,7 @@ export default function SecondarySidebar() {
 
   return (
     <div className="w-[300px] border-l-[1px] border-zinc-300 sticky top-0 h-screen pl-3 max-[1180px]:hidden">
-      <input
-        type="text"
-        onChange={(e) => setSearchTerm(e.target.value)}
-        className="border-[1px] w-full rounded-md pl-2 py-1 my-2"
-        placeholder="Search Users..."
-      />
-      <div className="flex flex-col gap-2">
+      <div className="flex flex-col gap-2 my-4">
         {usersDataLoading && (
           <div>
             <img
@@ -64,7 +46,7 @@ export default function SecondarySidebar() {
             />
           </div>
         )}
-        {usersList?.slice(0, 5)?.map((user) => {
+        {suggestedUsers?.slice(0, 5)?.map((user) => {
           return (
             <div
               key={user.userName}
@@ -76,7 +58,7 @@ export default function SecondarySidebar() {
               >
                 <img
                   src={user.avatar}
-                  className="w-9 rounded-full"
+                  className="w-9 h-9 object-cover rounded-full"
                   alt="user avatar"
                 />
                 <div>
@@ -94,15 +76,15 @@ export default function SecondarySidebar() {
               <img
                 src={followIcon}
                 onClick={() => followUser(user.userName)}
-                className="w-7 p-1 bg-zinc-600 rounded-full hover:cursor-pointer"
+                className="w-7 bg-zinc-300 rounded-full hover:cursor-pointer"
                 alt=""
               />
             </div>
           );
         })}
-        {!usersDataLoading && usersList?.length === 0 && (
+        {!usersDataLoading && suggestedUsers?.length === 0 && (
           <div className="flex justify-between items-center">
-            No users found.
+            No suggested Users found
           </div>
         )}
       </div>
